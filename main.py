1ba7e484bc5cdedc53f46566734d9540fb899fc5
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2025-07-19 09:18:20 -0500

    change
    main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_condictions as EC
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 #https://sites.google.com/chromium.org/driver/
@@ -15,13 +15,15 @@ driver = webdriver.Chrome()  # Selenium Manager resuelve el driver
 
 driver.get("https://google.com")
 
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
+)
+
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
 input_element.clear()
 input_element.send_keys("tech with tim" + Keys.ENTER)
 
-WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
-)
+
 
 time.sleep(10)
 
