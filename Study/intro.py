f6c736de68026ac8ff74c6cbe5a9eb66d0860747
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2025-07-20 16:56:04 -0500

    change
    Intro

--- Study/intro.py
+++ Study/intro.py
@@ -1,3 +1,12 @@
-class Dog:
-    def __init__(self):
-        self.bark()
+def make_class(x):
+    class Dog:
+        def __init__(self, name):
+            self.name = name 
+
+        def print_value(self):
+            print(x)
+    
+    return Dog
+
+cls = make_class(10)
+print(cls)
