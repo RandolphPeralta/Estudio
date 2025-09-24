9e53094b9383ed9cc601b2a05cc0bbd243379334
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2025-09-24 16:40:28 -0500

    Update 05.js
    Update 05.js

--- study/05.js
+++ study/05.js
@@ -6,7 +6,13 @@
 let array = [2,5,7,15,-5,-100,55];
 
 function getMenorMayor(arr){
-    array_sort = sort(array)
+    let menor = arr[0];
+    let mayor = arr[0];
+    for (numero of arr){
+        menor = menor < numero ? menor: numero;
+        mayor = mayor > numero ? mayor: numero;
+    }
+    return [menor, mayor];
 
 }
 
