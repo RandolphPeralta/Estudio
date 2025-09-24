a4a8e99bf9f8aa0ae13ae9556778820ac2f9be75
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2025-09-23 22:04:00 -0500

    Summary
    Summary

---
+++ study/05.js
@@ -0,0 +1,14 @@
+/**
+ * Crear algoritmo que devuelva número
+ * menor  mayor de un array
+ */
+
+let array = [2,5,7,15,-5,-100,55];
+
+function getMenorMayor(arr){
+    array_sort = sort(array)
+
+}
+
+let numeros = getMenorMayor(array)
+console.log(numeros);
