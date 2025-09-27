2a9be610e42a25fb44b897cff3e9d66da5b0242e
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2025-09-26 22:05:35 -0500

    Summary
    Summary

--- study/10.js
+++ study/10.js
@@ -1,4 +1,22 @@
 /**
  * Crear array de longitud N y que sus elementos sean
- * n
+ * numeros de 1 a N
  */
+
+let longitud = 7;
+
+function crearArray(n){
+    if (n <= 0){
+        return [];
+    }
+    let arr = [];
+    for (let i =0; i < n; i++){
+        arr[i] = i+ 1;
+    }
+    
+    return arr;
+
+}
+
+let resultado = crearArray(longitud);
+console.log(resultado)
---
+++ study/intro.js
@@ -0,0 +1,6 @@
+let email = 'nicolo@holamundo.io';
+let name = 'Nicolas';
+let direccion = {
+    calle: 'Queen st',
+    numero: 15,
+}
