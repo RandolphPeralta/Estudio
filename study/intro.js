4bc1549108ecb81538c60a5780d82c8ebf478345
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2025-09-27 10:08:32 -0500

    Summary
    Summary

--- study/intro.js
+++ study/intro.js
@@ -1,6 +1,12 @@
-let email = 'nicolo@holamundo.io';
-let name = 'Nicolas';
-let direccion = {
-    calle: 'Queen st',
-    numero: 15,
+let user = {
+    email: 'nicolas@holamundo.io',
+    name: 'Nicolas',
+    direccion:{
+        calle: 'Queen st',
+        numero: 15,
+    },
+    activo: true,
+    recuperarClave: function () {
+        console.log('Recuperando clave ...')
+    }
 }
