7f1628b00fc159ecc6adbb55b67adc3a064e6f4d
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2025-10-02 10:01:56 -0500

    Update study.cs
    Update study.cs

--- study/study.cs
+++ study/study.cs
@@ -1,18 +1,25 @@
-struct EMPLOYEE
-{
+struct EMPLOYEE {
     char szFirstName[25];
-    char szLasttName[25];
-
+    char szLastName[25];
     int iAge;
-
     double dPayRate;
-}
+};
 
-void main()
-{
+void main() {
     double dTotalPay;
+    struct EMPLOYEE *pEmp;
+    pEmp = (struct EMPLOYEE*)malloc(sizeof(struct EMPLOYEE));
 
-    struct EMPLOYEE*pEmp;   
-    pEmp = (struct EMPLOYEE*)malloc  
+    if (pEmp) {
+        pEmp->dPayRate = 100;
+        strcpy(pEmp->szFirstName, "Amy");
+        strcpy(pEmp->szLastName, "Anderson");
+        pEmp->iAge = 28;
 
+        dTotalPay = pEmp->dPayRate * 40;
+        printf("La paga total para %s %s es %.2f\n",
+                pEmp->szFirstName, pEmp->szLastName, dTotalPay);
+
+        free(pEmp);
+    }
 }
