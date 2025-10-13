fbdb2f17b97f8403240d6089edefb8542ad9eb41
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2025-10-12 22:00:33 -0500

    Summary
    Summary

--- study/prueba/obj/Debug/net9.0/prueba.AssemblyInfo.cs
+++ study/prueba/obj/Debug/net9.0/prueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58195e6ce3a740c75d8a007ebf144d255f56f09d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ddcafb08f78da68c38caa491ba6cf42b7e5a427")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
