e39e61d93cdbe058e8fe4efad437599ee919b203
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2025-10-23 17:02:25 -0500

    Summary
    Summary

---
+++ study/POO_py/clases_y_objetos.py
@@ -0,0 +1,8 @@
+class Celular():
+    def __init__(self)
+    #marca = "Samsung"
+    #modelo = "S23"
+    #camara = "48MP"
+
+#celular1 = Celular()
+#print(celular1.marca)
--- study/prueba/obj/Debug/net9.0/prueba.AssemblyInfo.cs
+++ study/prueba/obj/Debug/net9.0/prueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eecdef7efe8ca568d741fbe8e3d05b478fa85e02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+889aeb1d8449e94d8121b297ab6e4128710d2eca")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
