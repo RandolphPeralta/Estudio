d96e68a4f6aed3b9f4ed75538c2399af1b106cba
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2025-10-24 21:58:39 -0500

    Summary
    Summary

--- bitakora/tarea_bk.py
+++ bitakora/tarea_bk.py
@@ -1,6 +1,7 @@
 class Calculadora:
-    def __init__(self):
-        pass
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
 
     def sumar(self, num1: float, num2: float) -> float:
         return num1 + num2
@@ -8,19 +9,20 @@ class Calculadora:
     def restar(self, num1: float, num2: float) -> float:
         return num1 - num2
 
-    def operacion(self, numero1, numero2):
+    def operacion(self, num1, num2):
         operacion = input("Que operacion desea: '+' ó '-' : ")
         if operacion == '+':
-            return self.sumar(numero1, numero2)
+            return self.sumar(num1, num2)
         elif operacion == '-':
-            return self.restar(numero1, numero2)
+            return self.restar(num1, num2)
         else:
             return "La operacion no es ni suma ni resta"
        
-calculadora = Calculadora()
-
 numero1 = float(input("Ingrese su primer numero: "))
 numero2 = float(input("Ingrese su segundo numero: "))
+
+calculadora = Calculadora(numero1, numero2)
+
 print(calculadora.operacion(numero1, numero2))
 
 
