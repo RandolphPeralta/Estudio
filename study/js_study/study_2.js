6b0393f0c0a6e7acd8bf63e7f06f810ba3e5f995
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2025-11-05 22:16:17 -0500

    Summary
    Summary

--- study/js_study/study_2.js
+++ study/js_study/study_2.js
@@ -1,4 +1,39 @@
 // Programacion orientado a objetos en Javascript
 
+// Casi todo es un objeto
+// console.log(typeof "Hello word".toUpperCase());
+// console.log(typeof 100);
+// console.log(typeof false);
 
+// console.log(typeof { });
+// console.log(typeof [ ]);
 
+// console.log(typeof new Date());
+// console.log(typeof new RegExp());
+
+// console.log(new Number(100));
+// console.log(new String("hello world"));
+// console.log(new Boolean(true));
+
+// Objeto Literal
+
+// console.log( {} );
+// let name = "Ryan";
+// let lastname 
+
+// Metodos
+
+// function showFullName(){
+//     return "Ryan Ray"
+// }
+
+const user = {
+    name: "ryan",
+    lastname: "ray",
+    age: 30,
+    showFullName() {
+        return "Ryan Ray"
+    }
+}
+
+console.log(user.showFullName());
