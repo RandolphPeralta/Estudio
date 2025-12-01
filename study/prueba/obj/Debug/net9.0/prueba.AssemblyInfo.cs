54dc704e51f40197abb562aab8b14bf340eb8f71
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2025-12-01 16:57:50 -0500

    Summary
    Summary

--- study/prueba/obj/Debug/net9.0/prueba.AssemblyInfo.cs
+++ study/prueba/obj/Debug/net9.0/prueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4670d4c17f8d215b716bec4f7b2432d74288ce2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c052567a9fddde609e2130649c339be340f1dfdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- study/ts_study/borrador.ts
+++ study/ts_study/borrador.ts
@@ -1,9 +1,9 @@
-// 1. Interfaces
 interface IIdentificable {
   getId(): number;
+  getTitulo(): string;
+  getAutor(): string;
 }
 
-// 2. Clase Libro 
 class Libro implements IIdentificable {
   private id: number;
   private titulo: string;
@@ -41,31 +41,24 @@ class Libro implements IIdentificable {
   }
 }
 
-// 4. Clase abstracta Usuario — base para herencia
 abstract class Usuario {
-  private nombre: string; // AHORA PRIVATE
+  private nombre: string; 
 
   constructor(nombre: string) {
     this.nombre = nombre;
   }
 
-  // Encapsulamiento mediante getter
   public getNombre(): string {
     return this.nombre;
   }
 
 }
 
-// =============================================================
-// 5. Clase Cliente que hereda de Usuario y aplica POLIMORFISMO
-// =============================================================
 class Cliente extends Usuario {
   private prestamos: Libro[] = [];
 
   public mostrarInfo(): void {
-    console.log(
-      `👤 Cliente: ${this.getNombre()}, Libros prestados: ${this.prestamos.length}`
-    );
+    //console.log(`👤 Cliente: ${this.getNombre()}, Libros prestados: ${this.prestamos.length}`);
   }
 
   public prestarLibro(libro: Libro): void {
@@ -73,25 +66,28 @@ class Cliente extends Usuario {
       libro.prestar();
       this.prestamos.push(libro);
     } else {
-      console.log(`⚠️ ${this.getNombre()} no puede prestar "${libro.getTitulo()}".`);
+      //console.log(`⚠️ ${this.getNombre()} no puede prestar "${libro.getTitulo()}".`);
     }
   }
 
   public devolverLibro(libro: Libro): void {
+    
     const index = this.prestamos.indexOf(libro);
 
     if (index !== -1) {
       libro.devolver();
       this.prestamos.splice(index, 1);
     } else {
-      console.log(`⚠️ ${this.getNombre()} no tenía prestado "${libro.getTitulo()}".`);
+      //console.log(`⚠️ ${this.getNombre()} no tenía prestado "${libro.getTitulo()}".`);
     }
   }
 }
 
-// =============================================================
-// 6. Programa principal (ejemplo de uso)
-// =============================================================
+class Bibliotecario extends Usuario{
+  
+}
+
+
 const libro1 = new Libro(1, "Cien Años de Soledad", "Gabo");
 const libro2 = new Libro(2, "El Principito", "Saint-Exupéry");
 
