4d906783fdfd60f302c2eb413758708a8ad06661
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-01-03 22:01:36 -0500

    Update apuntes.ts
    Update apuntes.ts

--- study/ts_study/apuntes.ts
+++ study/ts_study/apuntes.ts
@@ -23,31 +23,62 @@
 
 // INYECCION DE DEPENDENCIA
 
-interface IProduct {
+// interface IProduct {
 
-}
+// }
 
 // DIP
 
 // INVERSION DE DEPENDENCIA
 
-class Products implements IProduct {
+// class Products implements IProduct {
 
-    public createProduct(product: any): void{
+//     public createProduct(product: any): void{
 
-    }
-}
+//     }
+// }
 
-class Clientes {
+// class Clientes {
 
-}
+// }
  
-class UI {
-    constructor(private _product: Products){} // ACA SE TIENE LA INYECCION DE DEPENDENCIA
+// class UI {
+//     constructor(private _product: Products){} // ACA SE TIENE LA INYECCION DE DEPENDENCIA
 
-    createProduct(data:any): void{
-        this._product.createProduct(data)
-    }
-}
+//     createProduct(data:any): void{
+//         this._product.createProduct(data)
+//     }
+// }
 
-const UiApp = new UI(new Products)  // CUANDO SE INSTANCIA A UIAPP EL ESPERA UN ARGUMENTO EN SU CONSTRUCTOR EL CUAL ES PRODUCT, PERO LA INYECCION DE DEPENDENCIA SE APOYA DE UN ELEMENTO QUE SE LLAMA EL INVERSOR DE CONTROL Y ESTA RECIBIENDO A LA CLASE Y LA IDEA ES QUE RECIBA LA INTERFAZ
+// const UiApp = new UI(new Products)  // CUANDO SE INSTANCIA A UIAPP EL ESPERA UN ARGUMENTO EN SU CONSTRUCTOR EL CUAL ES PRODUCT, PERO LA INYECCION DE DEPENDENCIA SE APOYA DE UN ELEMENTO QUE SE LLAMA EL INVERSOR DE CONTROL Y ESTA RECIBIENDO A LA CLASE Y LA IDEA ES QUE RECIBA LA INTERFAZ
+
+// interface IProducts {
+//     createProduct(product: any): void
+// }
+
+// class ProductsUseCase {
+
+// }
+
+// class ProductsService implements IProducts {
+
+//     constructor(private _productUseCase: ProductsService){}
+
+//     public createProduct(product: any): void{
+
+//     }
+// }
+
+// class Clients{
+
+// }
+
+// class UI {
+//     constructor (private _product: IProducts){}
+
+//     createProduct(data:any): void{
+//         this._product.createProduct(data)
+//     }
+// }
+
+// const uiApp = new UI(new ProductsUseCase)
