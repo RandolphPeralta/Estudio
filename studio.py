0cfd0aec991953581851039aeb9ffec25df6d1c9
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-01-17 21:38:58 -0500

    Update studio.py
    Update studio.py

--- studio.py
+++ studio.py
@@ -6,16 +6,28 @@ class Vehiculo(ABC):
     def ir(self):
         pass
 
+    def detener(self):
+        pass
+
 class Coche(Vehiculo):
     def ir(self):
         print("Conduces el auto")
 
+    def detener(self):
+        print("Este coche esta detenido")
+
 class Motocicleta(Vehiculo):
     def ir(self):
         print("Andas en la moto")
+
+    def detener(self):
+        print("Esta motocicleta esta detenido")
 
 coche = Coche()
 motocicleta = Motocicleta()
 
 coche.ir()
 motocicleta.ir()
+
+coche.detener()
+motocicleta.detener()
