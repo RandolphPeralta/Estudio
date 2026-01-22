00de41126605b1662c02062148941971c902003c
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-01-21 21:59:56 -0500

    Summary
    Summary

--- prueba.ts
+++ prueba.ts
@@ -3,3 +3,4 @@ const eventsMap = new Map();
 // for (const [key, value] of eventsMap){}
 //const [[firstKey, firstValue]] = eventsMap
 
+const newMap = {...eventsMap}
--- study/prueba/obj/Debug/net9.0/prueba.AssemblyInfo.cs
+++ study/prueba/obj/Debug/net9.0/prueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dddaf627cc75768af2806e45b275ab973b232585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f208e44f991e96fdf2dac7f175c913a66ef52e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
