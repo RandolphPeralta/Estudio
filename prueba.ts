382e46ba7dd5e0a66440710054e4c1ef4d58aecf
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-01-25 16:06:48 -0500

    Update prueba.ts
    Update prueba.ts

--- prueba.ts
+++ prueba.ts
@@ -1,17 +1,44 @@
-interface Superpoder {
-    usar(): any
-}
-
-class HeroeVolador implements Superpoder {
-    usar(){
-        return "Volando"
+//@ts-nocheck
+//#region Example 1
+// BAD - Too coupled
+class Products {
+    products = []
+    getAllProucts(){
+        const connection = new SQLConnection().get("Products");
+        return connection.data;
+        
     }
 }
 
-class App {
-    constructor(private poder: Superpoder){}
+// BETTER
 
-    activarPoder(){
-        return this.poder.usar()
+class Connection {
+    SQLConection = new SQLConection();
+    get(table:string){
+        return this.SQLConection.get(table);
     }
 }
+
+class Products {
+    products = []
+    constructor(connection: Connection)
+    getAllProucts(){
+        this.products = this.connection.get("proucts").data
+        return connection.data;
+        
+    }
+}
+//#endregion
+
+//#region Example 2
+class Task{}
+
+class TaskManager{
+    tasks: Task[];
+
+    
+
+}
+
+
+//#endregion
