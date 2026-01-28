b1ff66653f7b9dbec059c59885a773e0a09d1662
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-01-28 09:42:02 -0500

    Update prueba.ts
    Update prueba.ts

--- prueba.ts
+++ prueba.ts
@@ -234,7 +234,21 @@ grettingService.execute()
 // LISKOV SUBSTITUTION
 
 // BAD
+class Rectangle {
+    constructor(private width: number, private length: number){}
 
+    public setWidth(width: number){
+        this.width = width
+    }
+
+    public setLength(length: number){
+        this.length = length
+    }
+
+    public getArea() {
+        return this.width * this.length
+    }
+} 
 
 // INTERFACE SEGREGATION
 
