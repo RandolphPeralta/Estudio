e4e564d64e9eb1298de57eeb190eae67521c44d1
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-02-19 10:56:17 -0500

    Summary
    Summary

---
+++ prueba.ts
@@ -0,0 +1,26 @@
+function prompt(question: string): Promise<string> {
+  return new Promise((resolve) => {
+    process.stdout.write(question);
+
+    process.stdin.resume();
+    process.stdin.setEncoding("utf8");
+
+    const listener = (data: string) => {
+      process.stdin.pause();
+      process.stdin.removeListener("data", listener);
+      resolve(data.trim());
+    };
+
+    process.stdin.on("data", listener);
+  });
+}
+
+async function main() {
+  const nombre = await prompt("Nombre: ");
+  const edad = await prompt("Edad: ");
+
+  console.log(`Hola ${nombre}, tienes ${edad} años`);
+  process.exit();
+}
+
+main();
