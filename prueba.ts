d273f89e81b36555fbd7e4fb3988144768801dc6
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-02-19 21:58:00 -0500

    Update prueba.ts
    Update prueba.ts

--- prueba.ts
+++ prueba.ts
@@ -2,7 +2,8 @@ const sayHiFromFunction = (fn: Function) => {
   fn('Miguel')
 }
 
-sayHiFromFunction((name: string)=> {
-  console.log(`Hola ${name}`)
-})
+// const sayHi = ((name: string) => {
+//   console.log(`Hola ${name}`)
+// })
 
+// sayHiFromFunction(sayHi)
