81d9cbf2b0bacf2744b249591fff8f3ca7556866
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-02-24 09:40:24 -0500

    Update prueba.ts
    Update prueba.ts

--- prueba.ts
+++ prueba.ts
@@ -11,123 +11,121 @@ interface Gestion<T> {
   eliminar(item: T): boolean
   mostrar(): T[]
 }
-
 abstract class Empleado {
- abstract CalcularSalario(): number
+  abstract CalcularSalario(): number
 }
 
 interface Reporte {
   generar(): void
 }
 
-class Memoria<T> implements Gestion<T>{
+class Memoria<T> implements Gestion<T> {
   private memoria = new Set<T>()
 
   guardar(item: T): boolean {
     if (this.memoria.has(item)) return false
-    else (this.memoria.add(item)) 
+    this.memoria.add(item)
     return true
   }
 
   eliminar(item: T): boolean {
-    if (!this.memoria.delete(item)) return false
-    else (this.memoria.delete(item))
-    return true
+    return this.memoria.delete(item)
   }
 
   mostrar(): T[] {
-    return Array.from(this.memoria.values())
+    return Array.from(this.memoria)
+    // o: return [...this.memoria]
   }
 }
 
 //----------------------------
 
-class ReporteSalarios implements Reporte {
-  constructor(private empleados: Gestion<Empleado>) {}
+// class ReporteSalarios implements Reporte {
+//   constructor(private empleados: Gestion<Empleado>) {}
 
-  generar(): void {
-    const trabajadores = this.empleados.mostrar()
-    const salarios = trabajadores.map(empleado => ({
-      tipo: empleado.constructor.name,
-      salario: empleado.CalcularSalario()
-    }))
+//   generar(): void {
+//     const trabajadores = this.empleados.mostrar()
+//     const salarios = trabajadores.map(empleado => ({
+//       tipo: empleado.constructor.name,
+//       salario: empleado.CalcularSalario()
+//     }))
 
-    console.table(salarios)
-  }
-}
+//     console.table(salarios)
+//   }
+// }
 
-type Persona = {
-  cedula: string 
-  nombre: string
-};
+// type Persona = {
+//   cedula: string
+//   nombre: string
+// };
 
-type EmpleadoSalarioporHora = Persona & {
-  horas: number,
-  valor: number
-};
+// type EmpleadoSalarioporHora = Persona & {
+//   horas: number,
+//   valor: number
+// };
 
-type EmpleadoSalarioMensual = Persona & {
-  salario: number
-}
+// type EmpleadoSalarioMensual = Persona & {
+//   salario: number
+// }
 
-type EmpleadoContratista = Persona & {
-  valorproyecto: number
-}
+// type EmpleadoContratista = Persona & {
+//   valorproyecto: number
+// }
 
-class EmpleadoTiempoCompleto extends Empleado {
-  constructor(private persona: EmpleadoSalarioMensual) {super()}
+// class EmpleadoTiempoCompleto extends Empleado {
+//   constructor(private persona: EmpleadoSalarioMensual) {super()}
 
-  CalcularSalario(): number {
-    return this.persona.salario
-  }
-}
+//   CalcularSalario(): number {
+//     return this.persona.salario
+//   }
+// }
 
-class EmpleadoPorHoras extends Empleado {
-  constructor(private persona: EmpleadoSalarioporHora) {super()}
+// class EmpleadoPorHoras extends Empleado {
+//   constructor(private persona: EmpleadoSalarioporHora) {super()}
 
-  CalcularSalario(): number {
-    return this.persona.horas * this.persona.valor
-  }
-}
+//   CalcularSalario(): number {
+//     return this.persona.horas * this.persona.valor
+//   }
+// }
 
-class Contratista extends Empleado {
-  constructor(private persona: EmpleadoContratista) {super()}
+// class Contratista extends Empleado {
+//   constructor(private persona: EmpleadoContratista) {super()}
 
-  CalcularSalario(): number {
-    return this.persona.valorproyecto
-  }
-}
+//   CalcularSalario(): number {
+//     return this.persona.valorproyecto
+//   }
+// }
 
-const persona1: EmpleadoSalarioMensual = {
-  cedula: "123456",
-  nombre: "Carlos",
-  salario: 30000
-}
+// const persona1: EmpleadoSalarioMensual = {
+//   cedula: "123456",
+//   nombre: "Carlos",
+//   salario: 30000
+// }
 
-const persona2: EmpleadoSalarioporHora = {
-  cedula: "987654",
-  nombre: "Sara",
-  horas: 20,
-  valor: 5000
-}
+// const persona2: EmpleadoSalarioporHora = {
+//   cedula: "987654",
+//   nombre: "Sara",
+//   horas: 20,
+//   valor: 5000
+// }
 
-const persona3: EmpleadoContratista = {
-  cedula: "2001744",
-  nombre: "Ana",
-  valorproyecto: 70000
-}
+// const persona3: EmpleadoContratista = {
+//   cedula: "2001744",
+//   nombre: "Ana",
+//   valorproyecto: 70000
+// }
 
-const memoria = new Memoria<Empleado>()
+// const memoria = new Memoria<Empleado>()
 
-const empleado1: Empleado = new EmpleadoTiempoCompleto(persona1)
-const empleado2 = new EmpleadoPorHoras(persona2)
-const empleado3 = new Contratista(persona3)
+// const empleado1: Empleado = new EmpleadoTiempoCompleto(persona1)
+// const empleado2 = new EmpleadoPorHoras(persona2)
+// const empleado3 = new Contratista(persona3)
 
-memoria.guardar(empleado1)
-memoria.guardar(empleado2)
-memoria.guardar(empleado3)
+// memoria.guardar(empleado1)
+// memoria.guardar(empleado2)
+// memoria.guardar(empleado3)
 
-const reporte = new ReporteSalarios(memoria)
-reporte.generar()
+// const reporte = new ReporteSalarios(memoria)
+// reporte.generar()
 
 
