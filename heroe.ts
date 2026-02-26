32c69e3ccae6c75587c699a01d3d57e27cd6c525
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-02-26 09:33:27 -0500

    Update heroe.ts
    Update heroe.ts

--- heroe.ts
+++ heroe.ts
@@ -13,6 +13,8 @@ interface Villan {
     Destruir(): boolean
 }
 
+//---------------------------
+
 abstract class Character {
     constructor(private poder: ISuperpower){}
 
@@ -20,7 +22,69 @@ abstract class Character {
     abstract Destruir(): boolean
 }
 
-//---------------------------
+class Blowup implements ISuperpower {
+    use() {
+        return "Vuela por los cielos"
+    }
+}
+
+class Blowupwithsomething extends Blowup {
+    use() {
+        return "Vuela por los cielos con algo"
+    }
+}
+
+class Strengh implements ISuperpower {
+    use(){
+        return "SuperFuerza"
+    }
+}
+
+class Superman extends Character {
+
+    Salvar(): boolean {
+        return true
+    }
+    Destruir(): boolean {
+       return false
+    }
+}
+
+class Thanos extends Character {
+    
+    Salvar(): boolean {
+        return true
+    }
+    Destruir(): boolean {
+        return false
+    }
+    
+}
+
+const blowup = new Blowup()
+const volarcontraje: Blowup = new Blowupwithsomething()
+const fuerza = new Strengh()
+
+const superman: Hero = new Superman(blowup)
+const thanos: Villan = new Thanos(fuerza)
+
+
+
+
+
+
+
+// const Ironman: Hero = new Character(volarcontraje)
+// const Spiderman: Hero = new Character(trepamuros)
+// const DuendeVerde: Villan = new Character(volarcontraje)
+
+console.log(superman.Salvar())
+// console.log(Ironman.Salvar())
+
+console.log(thanos.Destruir())
+
+
+
 
 // class Character {
 //     constructor(private poder: ISuperpower) { }
@@ -36,82 +100,16 @@ abstract class Character {
 //     }
 // }
 
-
-
-
-class Blowup implements ISuperpower {
-    use() {
-        return "Vuela por los cielos"
-    }
-}
-
-class Blowupwithsomething extends Blowup {
-    use() {
-        return "Vuela por los cielos con algo"
-    }
-}
-
 class Speed implements ISuperpower {
     use() {
         return "Supervelocidad"
     }
 }
 
-class Strengh implements ISuperpower {
-    use(){
-        return "SuperFuerza"
-    }
-}
 
-class Supertelaraña implements ISuperpower {
-    use() {
-        return "Trepa muros con telaraña"
-    }
-}
 
 class Intelligence implements ISuperpower {
     use(): string {
-        return "Muy Inteligente"
+        return "SuperInteligencia"
     }
 }
-
-class Superman extends Character {
-    Salvar(): boolean {
-        return true
-    }
-    Destruir(): boolean {
-       return false
-    }
-}
-
-class Thanos extends Character {
-    Salvar(): boolean {
-        return true
-    }
-    Destruir(): boolean {
-        return false
-    }
-    
-}
-
-const blowup = new Blowup()
-const volarcontraje: Blowup = new Blowupwithsomething()
-const trepamuros = new Supertelaraña()
-const fuerza = new Strengh()
-
-
-const superman: Hero = new Superman(blowup)
-// const Ironman: Hero = new Character(volarcontraje)
-// const Spiderman: Hero = new Character(trepamuros)
-// const DuendeVerde: Villan = new Character(volarcontraje)
-
-const thanos: Villan = new Thanos(fuerza)
-
-console.log(superman.Salvar())
-// console.log(Ironman.Salvar())
-
-console.log(thanos.Destruir())
-
-
-
-
