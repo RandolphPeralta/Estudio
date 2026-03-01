8524ea6089cfe75c824a2ababa1a35c712841606
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-02-28 19:47:55 -0500

    Summary
    Summary

--- tarea.ts
+++ tarea.ts
@@ -1,9 +1,9 @@
 // Una empresa quiere crear solicitudes de acceso, para Aprobar, Rechazar, y Mostrar solicitudes
 
-interface Repositorio<T> {
-    guardar(item: T): boolean
-    eliminar(item: T): boolean
-    obtenerTodos(): T[]
+interface Accion {
+    guardar<T>(item: T): boolean
+    eliminar<T>(item: T): boolean
+    obtenerTodos<T>(): T[]
 }
 
 interface SistemaSolicitud<T> {
@@ -13,68 +13,77 @@ interface SistemaSolicitud<T> {
 
 //-----------------------
 
-class MemoriaRepositorio<T> implements Repositorio<T> {
+class MemoriaAccion implements Accion {
 
-    private datos = new Set<T>()
+    private datos = new Set()
 
-    guardar(item: T): boolean {
+    guardar<T>(item: T): boolean {
         this.datos.add(item)
         return true
     }
 
-    eliminar(item: T): boolean {
-        this.datos.delete(item)
-        return false
+    eliminar<T>(item: T): boolean {
+        return this.datos.delete(item)
     }
 
-    obtenerTodos(): T[] {
-        return Array.from(this.datos.values())
-    }
-}
-
-class Solicitudes<T> {
-    constructor(private repositorio: Repositorio<T>) { }
-
-    Aprobada(item: T) {
-        return this.repositorio.guardar(item)
-    }
-
-    Desaprobada(item: T) {
-        return this.repositorio.eliminar(item)
-    }
-
-    MostrarInfo() {
-        return this.repositorio.obtenerTodos()
+    obtenerTodos<T>(): T[] {
+        return Array.from(this.datos.values()) as T[]
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+// class Solicitudes<T> {
+//     constructor(private Accion: Accion<T>) { }
+
+//     Aprobada(item: T) {
+//         return this.Accion.guardar(item)
+//     }
+
+//     Desaprobada(item: T) {
+//         return this.Accion.eliminar(item)
+//     }
+
+//     MostrarInfo() {
+//         return this.Accion.obtenerTodos()
+//     }
+// }
 
 //------------------------
 
-type Solicitud = {
-    id: string
-    nombre: string
-}
+// type Solicitud = {
+//     id: string
+//     nombre: string
+// }
 
-type SolicitudInformacion = Solicitud & {
-    documento: string
-}
+// type SolicitudInformacion = Solicitud & {
+//     documento: string
+// }
 
-type SolicitudTecnologica = Solicitud & {
-    recurso: string
-}
+// type SolicitudTecnologica = Solicitud & {
+//     recurso: string
+// }
 
-const repositorio: Repositorio<Solicitud> = new MemoriaRepositorio<Solicitud>()
-const solicitudes: SistemaSolicitud<Solicitud> = new Solicitudes(repositorio)
+// const Accion: Accion<Solicitud> = new MemoriaAccion<Solicitud>()
+// const solicitudes: SistemaSolicitud<Solicitud> = new Solicitudes(Accion)
 
-const solicitud1: Solicitud = {
-    id: "1",
-    nombre: "Randolph"
-}
+// const solicitud1: Solicitud = {
+//     id: "1",
+//     nombre: "Randolph"
+// }
 
-const solicitud2: Solicitud = {
-    id: "2",
-    nombre: "Sara"
-}
+// const solicitud2: Solicitud = {
+//     id: "2",
+//     nombre: "Sara"
+// }
 
-solicitudes.Aprobada(solicitud1)
-solicitudes.Desaprobada(solicitud2)
+// solicitudes.Aprobada(solicitud1)
+// solicitudes.Desaprobada(solicitud2)
