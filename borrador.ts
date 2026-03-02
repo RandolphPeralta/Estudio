d3bb421d72d68bc63e44feb5fb10442a71602963
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-03-02 10:01:44 -0500

    Upate borrador.ts
    Upate borrador.ts

--- borrador.ts
+++ borrador.ts
@@ -1,15 +1,45 @@
-// Esto no cumple con OCP, Pero si SRP?
+// Sistema de domicilio, este software va a guardar ordenes y decir si se lograron entregar o no
 
-interface Ejecucion{
-    ejecutar(): any
+interface BaseDatos {
+    guardar(item: any): void
+    mostrar(): any
 }
 
-class View implements Ejecucion{
-    ejecutar(){
-        this.accion1()
+interface MemoriaRAM extends BaseDatos{
+    actualizar(item: any): void
+}
+
+interface MYSQL extends BaseDatos{
+    eliminar(item: any): void
+}
+
+class Ram implements MemoriaRAM {
+    guardar(item: any): void {
+        throw new Error("Method not implemented.")
     }
-
-    private accion1(){
-
+    mostrar() {
+        throw new Error("Method not implemented.")
+    }
+    actualizar(item: any): void {
+        throw new Error("Method not implemented.")
     }
 }
+
+class Mysql implements  MYSQL {
+    guardar(item: any): void {
+        throw new Error("Method not implemented.")
+    }
+    mostrar() {
+        throw new Error("Method not implemented.")
+    }
+    eliminar(item: any): void {
+        throw new Error("Method not implemented.")
+    }
+}
+
+class Sistema {
+    constructor(private memoria: BaseDatos){}
+}
+
+const basededatos = new Ram()
+
