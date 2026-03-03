38a778ec97c8434c130dba273f2e00f6632c9c81
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-03-03 17:33:21 -0500

    Update solid.ts
    Update solid.ts

--- solid.ts
+++ solid.ts
@@ -44,6 +44,25 @@ class FanFav implements Fav {
     }
 }
 
-const fan: Switch = new FanFav() // Aqui puedo presentar los principios SRP,  
+class DIPSwitch {
+    constructor(private devace: Switch){}
+}
+
+class DIPfav {
+    constructor(private devace: Fav){}
+}
+
+class DIPLight {
+    constructor(private device: Light){}
+}
+
+// Aqui puedo presentar los principios SRP, OCP, LSP
+
+const fanfav = new FanFav()
+const fanlight: Switch = new FanLight()
+const dip = new DIPSwitch(fanfav)
+
+
+// Falta como puedo colocar el principio ISP (aunque ya colocando el contrato Switch al lado de fan ya cumpliria, pero bueno), DIP
 
 
