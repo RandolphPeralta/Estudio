a0d5b62b6aaaec364322644bb637da2a27815894
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-03-05 16:40:31 -0500

    Update borrador.ts
    Update borrador.ts

--- borrador.ts
+++ borrador.ts
@@ -5,7 +5,7 @@ interface AutomovilBase {
     Apagar(): boolean
 }
 
-interface Nissan extends AutomovilBase{
+interface Nissan extends AutomovilBase{ 
     Estacionar(): void
 }
 
@@ -66,107 +66,9 @@ class AutoTesla {
     }
 }
 
-const disenioNissan: Nissan= new DisenioNissan() // SRP, OCP
-const disenioTesla: Tesla = new DisenioTesla() // SRP, OCP
-const disenio = new DisenioNissan() // LSP
-const otrodisenioTesla: AutomovilBase = new DisenioTesla() //, ISP
-const autoNissan = new AutoNissan(disenioNissan) // DIP
-const dautoTesla = new AutoTesla(disenioTesla) // DIP
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Sistema de domicilio, este software va a guardar ordenes y decir si se lograron entregar o no
-
-interface IBaseDatos {
-    guardar<T>(item: T): void
-    mostrar(): any
-}
-
-interface IMemoriaRAM extends IBaseDatos{
-    actualizar<T>(item: T): void
-}
-
-interface IMYSQL extends IBaseDatos{
-    eliminar<T>(item: T): void
-}
-
-//----------------------------
-
-class Ram implements IMemoriaRAM {
-
-    guardar(item: any): void {
-        throw new Error("Method not implemented.")
-    }
-    mostrar() {
-        throw new Error("Method not implemented.")
-    }
-    actualizar(item: any): void {
-        throw new Error("Method not implemented.")
-    }
-}
-
-class Mysql implements  IMYSQL {
-
-    guardar(item: any): void {
-        throw new Error("Method not implemented.")
-    }
-    mostrar() {
-        throw new Error("Method not implemented.")
-    }
-    eliminar(item: any): void {
-        throw new Error("Method not implemented.")
-    }
-}
-
-// LA INVERSION DE DEPENDENCIA LO QUE HARIA SERIA CREAR CADA DISEÑO, 
-// ES DECIR CREAR UNA CLASE PARA CADA ABSTRACION, 
-// PARA RESPETAR EL PRINCIPIO DE LISKOV?
-
-class Sistema {
-    constructor(){}
-}
-
-const basededatos = new Mysql()
+const disenioNissan: Nissan= new DisenioNissan()
+const disenioTesla: Tesla = new DisenioTesla()  
+const disenio = new DisenioNissan() 
+const otrodisenioTesla: AutomovilBase = new DisenioTesla()
+const autoNissan = new AutoNissan(disenioNissan) 
+const dautoTesla = new AutoTesla(disenioTesla)
