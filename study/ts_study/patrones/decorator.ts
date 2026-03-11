1cf48af301cb4f619acdde53574fe251ecab9574
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-03-11 16:13:20 -0500

    Update decorator.ts
    Update decorator.ts

--- study/ts_study/patrones/decorator.ts
+++ study/ts_study/patrones/decorator.ts
@@ -3,49 +3,32 @@
 // a un objeto sin modificar 
 // su clase original.
 
-
 // NOTA: ARREGLAR ESTE EJERCICIO 
 // CON SOLID, POO Y LA ABSTRACCION
 interface Cafe {
-    costo(): number
     descripcion(): string
 }
 
-class CafeSimple implements Cafe {
+abstract class DisenioCafe implements Cafe {
+    constructor(protected cafe: Cafe) { }
+    abstract descripcion(): string
+}
 
-    costo(): number {
-        return 5
-    }
+class CafeSimple implements Cafe {
 
     descripcion(): string {
         return "Café simple"
     }
 }
 
-abstract class DecoradorCafe implements Cafe {
-
-    constructor(protected cafe: Cafe) {}
-
-    abstract costo(): number
-    abstract descripcion(): string
-}
-
-class Leche extends DecoradorCafe {
-
-    costo(): number {
-        return this.cafe.costo() + 2
-    }
+class Leche extends DisenioCafe {
 
     descripcion(): string {
         return this.cafe.descripcion() + " + leche"
     }
 }
 
-class Azucar extends DecoradorCafe {
-
-    costo(): number {
-        return this.cafe.costo() + 1
-    }
+class Azucar extends DisenioCafe {
 
     descripcion(): string {
         return this.cafe.descripcion() + " + azúcar"
@@ -58,4 +41,3 @@ cafe = new Leche(cafe)
 cafe = new Azucar(cafe)
 
 console.log(cafe.descripcion())
-console.log(cafe.costo())
