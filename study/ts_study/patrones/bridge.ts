1fc2f1060ba71cd78cac9169d5b870cdf96f0876
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-03-17 23:05:08 -0500

    Summary
    Summary

---
+++ study/ts_study/patrones/bridge.ts
@@ -0,0 +1,64 @@
+// BRIDGE
+// permite dividir una clase grande, o un grupo de clases estrechamente 
+// relacionadas, en dos jerarquías separadas (abstracción e implementación) 
+// que pueden desarrollarse independientemente la una de la otra.
+
+// Implementación (abstracción del dispositivo → DIP)
+interface Dispositivo {
+    encender(): void
+    apagar(): void
+    setVolumen(vol: number): void
+}
+
+// Implementaciones concretas
+class TV implements Dispositivo {
+
+    encender(): void {
+        console.log("TV encendida")
+    }
+
+    apagar(): void {
+        console.log("TV apagada")
+    }
+
+    setVolumen(vol: number): void {
+        console.log("Volumen TV:", vol)
+    }
+}
+
+class Radio implements Dispositivo {
+
+    encender(): void {
+        console.log("Radio encendida")
+    }
+
+    apagar(): void {
+        console.log("Radio apagada")
+    }
+
+    setVolumen(vol: number): void {
+        console.log("Volumen Radio:", vol)
+    }
+}
+
+// Abstracción (Control remoto)
+class ControlRemoto {
+
+    constructor(protected dispositivo: Dispositivo) {}
+
+    encender() {
+        this.dispositivo.encender()
+    }
+
+    apagar() {
+        this.dispositivo.apagar()
+    }
+}
+
+// Abstracción refinada
+class ControlAvanzado extends ControlRemoto {
+
+    subirVolumen() {
+        this.dispositivo.setVolumen(100)
+    }
+}
---
+++ study/ts_study/patrones/composite.ts
@@ -0,0 +1,8 @@
+// COMPOSITE
+// compone objetos en estructuras de árbol 
+// y trabajar con esas estructuras como si 
+// fueran objetos individuales.
+
+interface Componente {
+    mostrar(indice: string): void
+}
