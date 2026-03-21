a96849bfa31060d9381e09ee572f3c78e52150d0
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-03-20 22:31:09 -0500

    Update command.ts
    Update command.ts

--- study/ts_study/patrones/command.ts
+++ study/ts_study/patrones/command.ts
@@ -1,10 +1,6 @@
 // COMMAND
 // convierte una solicitud en un objeto independiente que 
 // contiene toda la información sobre la solicitud. 
-// Esta transformación te permite parametrizar los métodos 
-// con diferentes solicitudes, retrasar o poner en cola 
-// la ejecución de una solicitud y soportar operaciones 
-// que no se pueden realizar.
 
 interface Command {
     ejecutar(): void
