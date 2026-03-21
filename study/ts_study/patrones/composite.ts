ad6af563fb0c802ba8dd501c8052429f9507fe4f
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-03-20 22:03:14 -0500

    Update composite.ts
    Update composite.ts

--- study/ts_study/patrones/composite.ts
+++ study/ts_study/patrones/composite.ts
@@ -3,6 +3,37 @@
 // y trabajar con esas estructuras como si 
 // fueran objetos individuales.
 
+// Componente (abstracción → clave del patrón)
 interface Componente {
     mostrar(indice: string): void
+}
+
+// Hoja (Leaf)
+class Archivo implements Componente {
+
+    constructor(private nombre: string) {}
+
+    mostrar(indice: string): void {
+        console.log(indice + "- Archivo: " + this.nombre)
+    }
+}
+
+// Compuesto (Composite)
+class Carpeta implements Componente {
+
+    private elementos: Componente[] = []
+
+    constructor(private nombre: string) {}
+
+    agregar(componente: Componente): void {
+        this.elementos.push(componente)
+    }
+
+    mostrar(indice: string): void {
+        console.log(indice + "+ Carpeta: " + this.nombre)
+
+        for (const elemento of this.elementos) {
+            elemento.mostrar(indice + "  ")
+        }
+    }
 }
