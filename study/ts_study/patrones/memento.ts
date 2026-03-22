430a679dddb9ee0b4b3c52dfbce2a2e3da43ec00
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-03-22 09:13:41 -0500

    Update memento.ts
    Update memento.ts

--- study/ts_study/patrones/memento.ts
+++ study/ts_study/patrones/memento.ts
@@ -2,3 +2,61 @@
 // Permite guardar y restaurar el estado previo 
 // de un objeto sin revelar los detalles de su implementación.
 
+class EditorMemento {
+
+    constructor(private contenido: string) {}
+
+    getContenido(): string {
+        return this.contenido
+    }
+}
+
+class Editor {
+
+    private contenido: string = ""
+
+    escribir(texto: string) {
+        this.contenido += texto
+    }
+
+    mostrar() {
+        return this.contenido
+    }
+
+    guardar(): EditorMemento {
+        return new EditorMemento(this.contenido)
+    }
+
+    restaurar(memento: EditorMemento) {
+        this.contenido = memento.getContenido()
+    }
+}
+
+class Historial {
+
+    private historial: EditorMemento[] = []
+
+    guardar(memento: EditorMemento) {
+        this.historial.push(memento)
+    }
+
+    deshacer(): EditorMemento | undefined {
+        return this.historial.pop()
+    }
+}
+
+const editor = new Editor()
+const historial = new Historial()
+
+editor.escribir("Hola ")
+historial.guardar(editor.guardar())
+
+editor.escribir("mundo")
+editor.mostrar()
+
+const memento = historial.deshacer()
+if (memento) {
+    editor.restaurar(memento)
+}
+
+editor.mostrar()
