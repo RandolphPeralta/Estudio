f79da6096a1ee23dec0fefe3e5bba23b4d69596d
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-04-11 19:30:48 -0500

    Update apuntes.js
    Update apuntes.js

--- apuntes.js
+++ apuntes.js
@@ -1,11 +1,26 @@
-var confirmacion = window.confirm("¿Quieres matricularte?")
+var fechaIngreso = prompt("Ingrese la fecha", "mm/dd/yyyy")
 
-if (confirmacion == true){
-    alert("Muchas gracias por matricularse")
-} 
-else {
-    alert("Ok, la matricula no se hizo")
-}
+
+
+// var valor1 = parseInt(window.prompt("Ingrese un numero"));
+// var valor2 = parseInt(window.prompt("Ingrese otro numero"));
+
+// if ( valor1 > valor2) {
+//     alert("El numero mayor es el valor: " + valor1)
+// }
+
+// else {
+//     alert("El numero mayor es " + valor2)
+// }
+
+// var confirmacion = window.confirm("¿Quieres matricularte?")
+
+// if (confirmacion == true){
+//     alert("Muchas gracias por matricularse")
+// } 
+// else {
+//     alert("Ok, la matricula no se hizo")
+// }
 
 // var respuesta = window.prompt("Ya te matriculaste?", "Si ya estoy matriculado");
 
