77eb43e65c6f35d5f12163e99fe95e1674d32c5b
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-04-13 17:43:55 -0500

    Update apuntes.js
    Update apuntes.js

--- apuntes.js
+++ apuntes.js
@@ -1,37 +1,42 @@
-let nombrePkm = ['Charmander', 'Pikachu', 'Bulbasaur', 'Squirtle']
 
-let habilidades = [
-    [80, 75, 90],
-    [65, 55, 95],
-    [80, 70, 65],
-    [85, 90, 70]
-];
 
-function calcularPromHab(habilidades){
-    let promedios = [];
 
-    for (let i=0; i < habilidades.length; i++){
-        let fila = habilidades[i];
-        let suma = fila.reduce((total, habilidad)=> total+habilidad, 0);
-        promedios[i] = suma/fila.length;   
-    }
 
-    return promedios;
-}
+// let nombrePkm = ['Charmander', 'Pikachu', 'Bulbasaur', 'Squirtle']
 
-function evaluarAptitud(nombresPkm, promedios) {
+// let habilidades = [
+//     [80, 75, 90],
+//     [65, 55, 95],
+//     [80, 70, 65],
+//     [85, 90, 70]
+// ];
 
-    for (let i=0; i<promedios.length; i++){
-        if (promedios[i]>=70){
-            console.log("El pokemon " + nombresPkm[i]+ " Supera el promedio con: " + promedios[i]);
-        }
-        else {
-            console.log("El pokemon " + nombresPkm[i] + " No supera el promedio con: " + promedios[i])
-        }
-    }
-}
+// function calcularPromHab(habilidades){
+//     let promedios = [];
 
-let promedios = calcularPromHab(habilidades);
+//     for (let i=0; i < habilidades.length; i++){
+//         let fila = habilidades[i];
+//         let suma = fila.reduce((total, habilidad)=> total+habilidad, 0);
+//         promedios[i] = suma/fila.length;   
+//     }
+
+//     return promedios;
+// }
+
+// function evaluarAptitud(nombresPkm, promedios) {
+
+//     for (let i=0; i<promedios.length; i++){
+//         if (promedios[i]>=70){
+//             console.log("El pokemon " + nombresPkm[i]+ " Supera el promedio con: " + promedios[i]);
+//         }
+//         else {
+//             console.log("El pokemon " + nombresPkm[i] + " No supera el promedio con: " + promedios[i])
+//         }
+//     }
+// }
+
+// let promedios = calcularPromHab(habilidades);
+// evaluarAptitud(nombrePkm,promedios);
 
 // function sumar(num1, num2){
 //     let resultado;
