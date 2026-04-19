ad935d760676a4ddeb23fede22e35e3616e22dfe
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-04-18 22:45:37 -0500

    Update localstorage.ts
    Update localstorage.ts

--- localstorage.ts
+++ localstorage.ts
@@ -2,11 +2,46 @@ var lista = []
 const nombre = 'Randolph'
 localStorage.setItem('nombre', nombre)
 
-class LocalStorageMemoria {
-    private memoriaprueba = Array<any>
+class Memoria {
 
-    guardar<T>(key: T){
-        let clave = String(key)
-        localStorage.setItem(clave, JSON.stringify(this.memoriaprueba))
+    guardar<T>(key: string, value: T): void {
+        localStorage.setItem(key, JSON.stringify(value))
+    }
+
+    mostrar<T>(key: string): T | null {
+        let data = localStorage.getItem(key)
+
+        if (data) {
+            return JSON.parse(data)
+        }
+
+        return null
+    }
+
+    eliminar(key: string): void {
+        localStorage.removeItem(key)
+    }
+
+    actualizar<T>(key: string, nuevoValor: T): void {
+        if (localStorage.getItem(key)) {
+            localStorage.setItem(key, JSON.stringify(nuevoValor))
+        }
+    }
+
+    mostrarTodo(): any[] {
+        let datos: any[] = []
+
+        for (let i = 0; i < localStorage.length; i++) {
+            let key = localStorage.key(i)
+
+            if (key) {
+                let value = localStorage.getItem(key)
+                if (value) {
+                    datos.push(JSON.parse(value))
+                }
+            }
+        }
+
+        return datos
     }
 }
