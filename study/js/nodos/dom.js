2729d097163023c9fe0e9c6e4b4737f641a53d7b
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-04-25 14:21:57 -0500

    Update dom.js
    Update dom.js

--- study/js/nodos/dom.js
+++ study/js/nodos/dom.js
@@ -1,3 +1,9 @@
 var documento = document
 
 document.appendChild('Modelos nuevos', document.createElement('p'))
+
+const lista_nodos = documento.querySelector('img')
+
+const matriz_nodos = [...lista_nodos]
+
+matriz_nodos.map()
