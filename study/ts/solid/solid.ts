32f5c728098dd31b8554c292cfa39bc3c7605f0c
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-05-02 15:27:16 -0500

    Summary
    Summary

--- study/ts/solid/solid.ts
+++ study/ts/solid/solid.ts
@@ -1,5 +1,69 @@
 // S - SINGLE RESPONSABILITY
 
+//  EJEMPLO MOSTRADO AL INSTRUCTOR Y FUE EXCELENTE
+
+// Sistema para crear un automovil
+
+interface AutomovilBase {
+    Encender(): boolean
+    Apagar(): boolean
+}
+
+interface Nissan extends AutomovilBase {
+    AutoEstacionar(): void
+}
+
+interface Toyota extends AutomovilBase {
+    AutoRepararse(): void
+}
+
+class DisenioNissan implements Nissan {
+    AutoEstacionar(): void {
+        
+    }
+
+    Encender(): boolean {
+        return true
+    }
+
+    Apagar(): boolean {
+        return false
+    }
+}
+
+class DisenioToyota implements Toyota {
+    AutoRepararse(): void { }
+
+    Encender(): boolean {
+        return true
+    }
+
+    Apagar(): boolean {
+        return false
+    }
+}
+
+class Fabrica {
+
+    constructor(private disenioauto: AutomovilBase) { }
+
+    probarAuto(): void {
+        this.disenioauto.Encender()
+        this.disenioauto.Apagar()
+    }
+}
+
+const disenionissan = new DisenioNissan()
+const otrodisenioToyota: AutomovilBase = new DisenioToyota()
+const auto = new Fabrica(disenionissan)
+
+
+
+
+
+
+
+
 // Una clase debe tener una unica responsabilidad 
 // Es decir una unica funcion 
 // y por ende una razon para cambiar
