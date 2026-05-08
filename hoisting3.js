1d88b54d8311170c9f6cf70e96d2ee8b14e8cfdd
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-05-08 16:30:41 -0500

    Update hoisting3.js
    Update hoisting3.js

--- hoisting3.js
+++ hoisting3.js
@@ -1,1 +1,26 @@
 // Toca colocar otro ejemplo
+
+// Global scope ó Modulo Scope
+// Local Scope o Function Scope
+// Block Scope
+
+// Scope 
+
+// Global
+var username = "Tzuzul"
+function sayHi(){
+
+    // Local o Function Scope
+    var username = "Code" // variable local
+    console.log(username)
+    // block scope: let y const
+    if(true){
+        let fullname = "Tzuzul code"
+        console.log(fullname)
+    }
+
+    console.log(fullname)
+
+}
+
+sayHi();
