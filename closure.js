d2c213661ba1492d99898f14767ee20dd63c96c3
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-05-12 17:46:47 -0500

    Update closure.js
    Update closure.js

--- closure.js
+++ closure.js
@@ -8,3 +8,32 @@ const saludarHola = crearSaludo('Hola');
 saludarHola('Randolph');
 
 // CONSULTAR UN POCO MAS DE CLOUSURE
+
+const miContador = (
+  function(){
+  var _contador = 0;
+
+  function incrementar(){
+    return ++_contador;
+  }
+
+  function decrementar(){
+    return --_contador;
+  }
+
+  function valor(){
+    return _contador
+  }
+
+  return {
+    valor,
+    incrementar,
+    decrementar,
+    
+  }
+})();
+
+console.log(miContador.valor());        
+console.log(miContador.incrementar());  
+console.log(miContador.valor()); 
+console.log(miContador.decrementar());
