32cbbf7d30709af4218f351d478ebe8c78ba5ef1
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-07-10 19:29:22 -0500

    Summary
    Summary

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,29 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.ts", // tu único punto de entrada
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".ts", ".js"]
-  },
+    entry: "./src/index.ts",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
 
-  devServer: {
-    directory: path.join(__dirname, "src/ui/web"),
-    open: true,
-    port: 8000
-  },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "src/ui/web")
+        },
+        open: true,
+        port: 8000
+    },
 };
