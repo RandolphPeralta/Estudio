f64f1d0eeeba4d733aad5672883e9ba05f89c42b
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-09-17 16:47:13 -0500

    Create training.ts
    Create training.ts

---
+++ src/study/ts/trainingweb/training.ts
@@ -0,0 +1,42 @@
+// DOM - Document Object Model
+// ambitos - var, let, y const
+// arrow functions
+// closure
+// callbacks
+// async/await
+// promise
+// XHR - AJAX
+// IIFE
+// Funcion de Declaración
+// Funcion de Expresion
+// Hoisting
+// getElementBy
+// call, bind, apply
+// HTML5
+// CSS
+// SASS
+// BEM
+
+class Persona {}
+
+class Empleado extends Persona{
+    constructor(private nombre: string, private apelido: string){
+        super()
+    }
+
+    miMetodo(){}
+
+    otroMetodo(){}
+
+}
+
+var OtraPersona = {
+    nombre: '',
+    apellido: '',
+    miMetodo: function(){
+        this.nombre
+    },
+    otroMetodo: () => {
+        console.log('hola')
+    }
+}
