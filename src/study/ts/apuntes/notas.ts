66bca691a76aad154ca2162b144d387838ed575a
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-09-17 19:17:26 -0500

    Update notas.ts
    Update notas.ts

---
+++ src/study/ts/apuntes/notas.ts
@@ -0,0 +1,6 @@
+let a = Symbol('a')         // symbol 
+let b: symbol = Symbol('b') // symbol 
+var c = a === b             // boolean 
+// let d = a + 'x'             // Error TS2469: The '+' operator cannot be 
+
+                            // to type 'symbol'
