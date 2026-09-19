ac268cffdae9ec0b2c90122849d36b659e2a471f
RandolphPeralta <107772807+RandolphPeralta@users.noreply.github.com>
2026-09-19 17:02:47 -0500

    Update notas.ts
    Update notas.ts

--- src/study/ts/apuntes/notas.ts
+++ src/study/ts/apuntes/notas.ts
@@ -4,3 +4,17 @@ var c = a === b             // boolean
 // let d = a + 'x'             // Error TS2469: The '+' operator cannot be 
 
                             // to type 'symbol'
+
+function* createFibonacciGenerator() {  
+    let a = 0 
+    let b = 1 
+        while (true) {  
+            yield a;  
+                [a, b] = [b, a + b]  
+            } 
+    } 
+
+let fibonacciGenerator = createFibonacciGenerator() // IterableIterator<number> 
+ 
+console.log(fibonacciGenerator.next())    // evaluates to {value: 0, done: false} 
+console.log(fibonacciGenerator.next())    // evaluates to {value: 1, done: false}
